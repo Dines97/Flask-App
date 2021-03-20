4ac201edcc37cbd465586c9cc2b7af608497868a
Denis Kaynar <kaynar.denis@gmail.com>
2021-03-20 14:45:17 +0300

    Added whoami

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+data = {'firstname': None,
+        'lastname': None}
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "My name is Denis Kaynar"
+
+
+@app.route('/whoami', methods=['GET'])
+def whoami():
+    data["firstname"] = request.args.get('firstname')
+    data["lastname"] = request.args.get('lastname')
+    return jsonify(data)
 
 
 if __name__ == "__main__":
