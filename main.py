0757ae4f829c5b53e286c86692268a02fb95e0df
Denis Kaynar <kaynar.denis@gmail.com>
2021-03-20 15:10:09 +0300

    Added alert

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
+
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+url = os.getenv('WEBHOOK_URL')
 data = {'firstname': None,
         'lastname': None}
 
@@ -16,6 +20,14 @@ def whoami():
     data["firstname"] = request.args.get('firstname')
     data["lastname"] = request.args.get('lastname')
     return jsonify(data)
+
+
+@app.route('/alert', methods=['POST'])
+def alert():
+    if request.is_json:
+        requests.post(url, headers={'Content-Type': 'application/json'}, json=request.get_json())
+    else:
+        return 'Bad Request', 400
 
 
 if __name__ == "__main__":
